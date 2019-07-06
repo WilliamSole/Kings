f939b6e363913f0715c5e5b02a80a364745940d9
BOB\Will <williamsolebo@gmail.com>
2019-07-06 15:45:58 +0100

    add addItem and removeItem fucntionality to checkoutItem

--- src/component/checkout-item/checkout-item.component.jsx
+++ src/component/checkout-item/checkout-item.component.jsx
@@ -1,17 +1,34 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
+import { clearItemFromCart, removeItem, addItem } from '../../redux/cart/cart.actions';
 
 import './checkout-item.styles.scss';
 
-const CheckoutItem = ({item: {name, quantity, price, imageUrl}}) => (
-    <div className='checkout-item'>
-        <div className='image-container'>
-            <img src={imageUrl} alt='item' />
-        </div>
-        <span className='name'>{name}</span>
-        <span className='quantity'>{quantity}</span>
-        <span className='price'>{price}</span>
-        <div className='remove-button'>&#10008;</div>
-    </div>
-);
+const CheckoutItem = ({ item, clearItem, removeItem, addItem }) => {
+    const { name, quantity, price, imageUrl } = item;
 
-export default CheckoutItem;
+    return (
+        <div className='checkout-item'>
+            <div className='image-container'>
+                <img src={imageUrl} alt='item' />
+            </div>
+            <span className='name'>{name}</span>
+            <span className='quantity'>
+                <div className='arrow' onClick={() => removeItem(item)}>&#10094;</div>
+                <span className='value'>{quantity}</span>
+                <div className='arrow' onClick={() => addItem(item)}>&#10095;</div>
+            </span>
+            <span className='price'>{price}</span>
+            <div className='remove-button' onClick={() => clearItem(item)}>&#10008;</div>
+        </div>
+    );
+};
+
+const mapDispatchToProps = dispatch => ({
+    clearItem: item => dispatch(clearItemFromCart(item)),
+    removeItem: item => dispatch(removeItem(item)),
+    addItem: item => dispatch(addItem(item))
+});
+
+export default connect(null, mapDispatchToProps)(CheckoutItem);
