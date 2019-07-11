bceb09ff04120fea2318cda168e62a1aeaf907a2
BOB\Will <williamsolebo@gmail.com>
2019-07-11 15:53:18 +0100

    sperate cra app into client folder, add epress server to handle backend stripe payments

--- client/src/component/stripe-button/stripe-button.component.jsx
+++ client/src/component/stripe-button/stripe-button.component.jsx
@@ -1,15 +1,27 @@
 import React from 'react';
+import axios from 'axios';
 import StripeCheckout from 'react-stripe-checkout';
-
-const onToken = token => {
-    console.log(token);
-    alert('Payment Sucessful');
-}
 
 // Adds stripe checkout functionality for payment handling
 const StripeCheckoutButton = ({ price }) => {
     const priceForStripe = price * 100;
     const publishableKey = 'pk_test_N6jzoWOrQ4Ojbb5mNnbzGvin00JWxDDvA9';
+
+    const onToken = token => {
+        axios({
+            url: 'payment',
+            method: 'post',
+            data: {
+                amount: priceForStripe,
+                token
+            }
+        }).then( res => {
+            alert('Payment Successful');
+        }).catch(err => {
+            console.log('Payment Unsuccessful', JSON.parse(err));
+            alert('There was an error with your payment. Please make sure you use the provided credit card.')
+        })
+    }
 
     return (
         <StripeCheckout
